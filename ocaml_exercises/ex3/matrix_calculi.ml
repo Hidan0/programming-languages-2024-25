b936bc06c158fbfb484f9e00e39999c5465a2b8b
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-10-09 16:10:32 +0200

    add, ex3: + and *

--- ocaml_exercises/ex3/matrix_calculi.ml
+++ ocaml_exercises/ex3/matrix_calculi.ml
@@ -18,7 +18,8 @@ let print_matrix (m : matrix) : unit =
         print_newline ();
         aux t
   in
-  aux m
+  aux m;
+  print_newline ()
 
 let zeros (n : int) (m : int) : matrix =
   List.init n (fun _ -> List.init m (fun _ -> 0))
@@ -29,14 +30,39 @@ let identity n m : matrix =
 let init n : matrix =
   List.init n (fun r -> List.init n (fun c -> (r * n) + c + 1))
 
-let () = print_matrix (zeros 2 4);;
+let rec transpose m =
+  match m with
+  | [] -> []
+  | [] :: row -> transpose row
+  | (hd :: tl) :: rest ->
+      (hd :: List.map List.hd rest) :: transpose (tl :: List.map List.tl rest)
 
-print_newline ()
+let ( ++ ) m1 m2 =
+  List.map2 (fun r1 r2 -> List.map2 (fun c1 c2 -> c1 + c2) r1 r2) m1 m2
 
-let () = print_matrix (identity 4 4);;
+(* Dot product *)
+let ( ** ) v1 v2 =
+  let rec dotprod acc v1 v2 =
+    match (v1, v2) with
+    | hd1 :: tl1, hd2 :: tl2 -> dotprod (acc + (hd1 * hd2)) tl1 tl2
+    | _, _ -> acc
+  in
+  dotprod 0 v1 v2
 
-print_newline ()
+let ( *** ) m1 m2 =
+  let matprod m1 m2 =
+    let cols2 = transpose m2 in
+    List.map (fun r1 -> List.map (fun c2 -> r1 ** c2) cols2) m1
+  in
+  matprod m1 m2
 
-let () = print_matrix (init 4);;
+let () = print_matrix (zeros 2 4)
+let () = print_matrix (identity 4 4)
+let () = print_matrix (init 4)
+let () = print_matrix (transpose (init 4))
+let () = print_matrix (init 4 ++ identity 4 4)
 
-print_newline ()
+(* let () = print_int ([ 1; 3; -5 ] ** [ 4; -2; -1 ]) *)
+(* let () = print_int ([ 1; 2; 3 ] ** [ 0; 1; 0 ]) *)
+let () = print_matrix (init 4 *** identity 4 4)
+let () = print_matrix (init 4 *** zeros 4 4)
