8751c2a3b1aa8a46930293f25f6e94e8f5a95860
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-11-07 18:02:34 +0100

    99p, p08

---
+++ erlang/99-problems/p08.erl
@@ -0,0 +1,11 @@
+-module(p08).
+-export([compress/1]).
+
+% p08:compress([1,1,1,2,2,3,4,4,4,4,4,4]). 
+%    [1,2,3,4]
+
+aux([H|T], Prev) when H == Prev -> aux(T, Prev);
+aux([H|T], _) -> [H] ++ aux(T, H);
+aux([], _) -> [].
+
+compress(Lst) -> aux(Lst, 0).
