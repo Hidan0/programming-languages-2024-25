ffad2b4935d951cde2895acd45a618f006741308
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-11-13 22:13:27 +0100

    p12

---
+++ erlang/99-problems/p12.erl
@@ -0,0 +1,13 @@
+-module(p12).
+-export([decode/1]).
+
+% Given a run-length code list generated as specified in problem P10, construct its uncompressed version.
+% Example:
+%
+% > p12:decode([{4,a},{1,b},{2,c},{2,a},{1,d},{4,e}]).
+%     [a,a,a,a,b,c,c,a,a,d,e,e,e,e]
+
+decode(Ls) -> lists:foldr(
+  fun({Count, El}, Acc) ->
+      lists:duplicate(Count, El) ++ Acc
+  end, [], Ls).
