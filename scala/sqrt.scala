899bfc529cc543fa6f0397ab9fd06f1c711e61de
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-12-12 15:01:40 +0100

    refactored sqrt.scala with scoped functions

--- scala/sqrt.scala
+++ scala/sqrt.scala
@@ -1,9 +1,13 @@
 object Sqrt {
   val EPSILON = 0.0001
 
-  private def abs(x: Double) = if (x >= 0) x else -x
-  private def isGood(guess: Double, x: Double): Boolean = abs(guess * guess - x) < EPSILON
-  private def sqrt(guess: Double, x: Double): Double = if (isGood(guess, x)) guess else sqrt((guess + x / guess) / 2, x)
+  def sqrt(x: Double): Double = {
+    def abs(x: Double) = if (x >= 0) x else -x
+    def isGood(guess: Double, x: Double): Boolean = abs(guess * guess - x) < EPSILON
+    def sqrt(guess: Double, x: Double): Double = 
+      if (isGood(guess, x)) guess 
+      else sqrt((guess + x / guess) / 2, x)
 
-  def sqrt(x: Double): Double = sqrt(x, x) 
+    sqrt(x, x) 
+  }
 }
