48b19fd3e32322b1122629b996e16cfe97934c25
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-12-19 18:59:46 +0100

    scala ex1, add, is_an_anagram

--- scala_exercises/lab01/ex1.scala
+++ scala_exercises/lab01/ex1.scala
@@ -3,12 +3,28 @@ object Ex1 {
     var parsed = str.map((c: Char) => c.toLower).filter((c: Char) => c.isLetter)
     parsed == parsed.reverse
   }
+
+  def is_an_anagram(str: String, dict: List[String]): Boolean = {
+    dict
+      .map((s: String) => s.toSeq.sorted.unwrap)
+      .filter((s: String) => s.equals(str.toSeq.sorted.unwrap))
+      .lengthIs >= 1
+  }
 }
 
 object Main extends App {
   List("detartrated", "Do geese see God?", "Rise to vote, sir.", "nope.")
     .map((input: String) =>
       f"is_palindrome(\"$input\") = ${Ex1.is_palindrome(input)}\n"
+    )
+    .foreach(print)
+
+  val dict =
+    List("incerta", "trincea", "cartine", "citarne", "pratesi", "espatrio", "tar")
+
+  List("", "carenti", "sparite", "luigi", "rat")
+    .map((input: String) =>
+      f"is_an_anagram(\"$input\") = ${Ex1.is_an_anagram(input, dict)}\n"
     )
     .foreach(print)
 }
