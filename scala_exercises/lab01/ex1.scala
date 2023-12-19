6fa4010f13a048202db7038dba89fca073fed47a
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-12-19 18:45:08 +0100

    scala ex1, refactored is_palindrome test

--- scala_exercises/lab01/ex1.scala
+++ scala_exercises/lab01/ex1.scala
@@ -1,13 +1,14 @@
 object Ex1 {
   def is_palindrome(str: String): Boolean = {
-   var parsed = str.map((c: Char) => c.toLower).filter((c: Char) => c.isLetter)
-   parsed == parsed.reverse
+    var parsed = str.map((c: Char) => c.toLower).filter((c: Char) => c.isLetter)
+    parsed == parsed.reverse
   }
 }
 
 object Main extends App {
-  printf("is_palindrome \"%s\" should be %b, got %b\n", "detartrated", true, Ex1.is_palindrome("detartrated"))
-  printf("is_palindrome \"%s\" should be %b, got %b\n", "Do geese see God?", true, Ex1.is_palindrome("Do geese see God?"))
-  printf("is_palindrome \"%s\" should be %b, got %b\n", "Rise to vote, sir.", true, Ex1.is_palindrome("Rise to vote, sir."))
-  printf("is_palindrome \"%s\" should be %b, got %b\n", "nope", false, Ex1.is_palindrome("nope"))
+  List("detartrated", "Do geese see God?", "Rise to vote, sir.", "nope.")
+    .map((input: String) =>
+      f"is_palindrome(\"$input\") = ${Ex1.is_palindrome(input)}\n"
+    )
+    .foreach(print)
 }
